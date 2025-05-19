23a950f52681ed53139ea87c441422dd3efe079d
Rev.Jake <rev.jake@gmail.com>
2025-05-19 16:41:48 -0500

    more ts tests

--- src/entities/items/baseItem/Dnd35eItemModel.ts
+++ src/entities/items/baseItem/Dnd35eItemModel.ts
@@ -1,44 +1,50 @@
 
 type DataSchema = foundry.data.fields.DataSchema;
+
 import { AnyObject } from "fvtt-types/utils";
 import Document = foundry.abstract.Document;
+import "fvtt-types";
 
-declare namespace ItemData {
-  interface Schema extends DataSchema {
-    // Currency
-    goldValue: foundry.data.fields.NumberField;
-    silverValue: foundry.data.fields.NumberField;
-    // actions
-    actionType: foundry.data.fields.StringField;
-  }
-}
+// declare namespace ItemData {
+//   interface Schema extends DataSchema {
+//     // Currency
+//     goldValue: foundry.data.fields.NumberField;
+//     silverValue: foundry.data.fields.NumberField;
+//     // actions
+//     actionType: foundry.data.fields.StringField;
+//   }
+// }
 
-export class Dnd35eItemModel extends foundry.abstract.TypeDataModel<ItemData.Schema, Item.Implementation> {
-  static defineSchema() {
-    const fields = foundry.data.fields;
-    return {
-      goldValue: new fields.NumberField({
-        required: true,
-        default: 0,
-        validate: (value) => {
-          if (value < 0) throw new Error("Gold value cannot be negative");
-          return true;
-        },
-      }),
-      silverValue: new fields.NumberField({
-        required: true,
-        default: 0,
-        validate: (value) => {
-          if (value < 0) throw new Error("Silver value cannot be negative");
-          return true;
-        },
-      }),
-      actionType: new fields.StringField({
-        required: false,
-        default: "",
-      }),
-    };
-  }
+const fields = foundry.data.fields;
+const schema = {
+  goldValue: new fields.NumberField({
+    required: true,
+    default: 0,
+    validate: (value) => {
+      if (value < 0) throw new Error("Gold value cannot be negative");
+      return true;
+    },
+  }),
+  silverValue: new fields.NumberField({
+    required: true,
+    default: 0,
+    validate: (value) => {
+      if (value < 0) throw new Error("Silver value cannot be negative");
+      return true;
+    },
+  }),
+  actionType: new fields.StringField({
+    required: false,
+    default: "",
+  }),
+};
+
+type Schema = typeof schema;
+
+class Dnd35eItemModel extends foundry.abstract.TypeDataModel<Schema, never> {
+    static defineSchema() {
+        return schema;
+    }
 }
 
 declare global {
